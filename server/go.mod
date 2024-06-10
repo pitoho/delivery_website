d791e7364ea97c790a9fe31f337c8d6a68d61ad4
pitoho <drobysheva_1995@list.ru>
2024-06-10 23:02:36 +0300

    register

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,8 @@
 module github.com/pitoho/delivery_website/server
 
 go 1.21.0
+
+require (
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -4,6 +4,8 @@ import (
     "fmt"
 	"log"
     "net/http"
+	"database/sql"
+    _ "github.com/lib/pq" 
 )
 
 
@@ -16,4 +18,23 @@ func main() {
 	log.Panic(
 		http.ListenAndServe(":3000", nil),
 	)
+
+	connStr := "user=postgres dbname=delivery_db password=babymonster sslmode=disable"
+    db, err := sql.Open("postgres", connStr)
+    if err != nil {
+        panic(err)
+    }
+    defer db.Close()
+
+    if err := db.Ping(); err != nil {
+        panic(err)
+    }
+
+    http.HandleFunc("/login", loginHandler(db))
+	http.HandleFunc("/register", registrationHandler(db))
+
+    fmt.Println("Сервер запущен на http://localhost:8080")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        panic(err)
+    }
 }
